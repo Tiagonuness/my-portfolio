ff14415f28f0c9c7e82f844231a29cce91a4c610
Tiago nunes <117403385+Tiagonuness@users.noreply.github.com>
2024-10-17 21:22:55 -0300

    redes sociais pronta, bg hero pronta, navbar 50%

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,120 +1,23 @@
 import Hero from "./sections/Hero/Hero";
-import { useCallback } from "react";
-import Particles from "react-particles";
-import type { Container, Engine} from "tsparticles-engine";
-import { loadSlim } from "tsparticles-slim";
 import {FaGithub, FaTwitter, FaLinkedin, FaInstagram,} from "react-icons/fa";
 import "./Home.css";
 
 const Home = () => {
-    const particlesInit = useCallback(async (engine: Engine) => {
-        await loadSlim(engine);
-    }, []);
-
-    const particlesLoaded = useCallback(async (container: Container | undefined) => {
-        await console.log(container);
-    }, []);
     return (
         <>
-            <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "#000000",
-                    },
-                },
-                fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
-                        onHover: {
-                            enable: true,
-                            mode: "grab",
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 4,
-                        },
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4,
-                        },
-                    },
-                },
-                particles: {
-                    color: {
-                        value: "#ffffff",
-                    },
-                    links: {
-                        color: "#ffffff",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: false,
-                        speed: 6,
-                        straight: false,
-                        out_mode: "out",
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.5,
-                        random: false,
-                        anim: {
-                            enable: false,
-                            speed: 1,
-                            opacity_min: 0.1,
-                            sync: false
-                        }
-                    },
-                    shape: {
-                        type: "circle",
-                        stroke: {
-                            width: 0,
-                            color: "#000000"
-                        },
-                        polygon: {
-                        nb_sides: 5
-                        }
-                    },
-                    size: {
-                        value: { min: 0.1, max: 5 },
-                        random: true,
-                        anim: {
-                            enable: false,
-                            speed: 40,
-                            size_min: 0.1,
-                            sync: false
-                        }
-                    },
-                },
-                detectRetina: true,
-                }}
-                style={{ position: "absolute", width: "100%", height: "100%", zIndex: 0 }}
-            />
+            <div id="navbar"  className="col-lg-6 mx-auto justify-content-center" >
+                <nav>
+                    <div id="bordaNavbar" className="mt-1 py-2 d-flex justify-content-center align-items-center">
+                        <ul className="list-unstyled d-flex">
+                            <li className="mx-3 ml-3"><a href="#">About</a></li>
+                            <li className="mx-3"><a href="#">Skills</a></li>
+                            <li className="mx-3"><a href="#">Projects</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
 
-            <div className="iconsPerfis col-lg-12">
+            <div id="iconsPerfis" className="col-lg-12">
                 <ul>
                     <li>
                         <a href="#" target="_blank">
@@ -140,7 +43,7 @@ const Home = () => {
                 <p>Me Siga</p>
             </div>
 
-            <div style={{ position: "relative", zIndex: 1 }}>
+            <div style={{ position: "relative", zIndex: -1 }}>
                 <Hero />
             </div>
         </>
