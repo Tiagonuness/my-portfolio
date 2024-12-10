c384068adc117bdc8aa7407a04ec684a5751c558
Tiago nunes <117403385+Tiagonuness@users.noreply.github.com>
2024-12-10 09:30:50 -0300

    About

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -6,46 +6,26 @@ const About = () => {
     return (
       <div id="aboutSection" className="col-lg-12 aboutSction">
         <div className="row col-lg-12">
-          <div id="tituloSobre" className="row col-lg-1 d-flex justify-content-center align-items-center vh-100">
-            <h2 className="text-center text-dark verticalTitulo" data-shadow="SOBRE">SOBRE</h2>
+          <div id="tituloSobre" className="col-lg-12 d-flex justify-content-center  ">
+            <h2 className="text-center verticalTitulo mt-5" data-shadow="SOBRE">SOBRE</h2>
           </div>
-          <div className="row col-lg-11 mt-4">
-            <div className="quadro col-lg-3" id="unb">
-              <div className="d-flex align-items-center">
-                <img src={logoUnB} alt="Universidade de Brasília"/>
-                <div className="d-flex flex-column">
-                  <p className="mb-0"><b>Faculdade:</b></p>
-                  <p><em>Universidade de Brasília</em></p>
-                </div>
-              </div>
-              <p className="mb-0"><b>Curso:</b> Engenharia da computação</p>
-              <p className="mb-0"><b>Início:</b> 2020.1</p>
-              <p><b>Conclusão:</b> 8º Semestre</p>
+          <div className="row col-lg-12 mt-4">
+            <div className="col-lg-1"></div>
+            <div className="col-lg-5" id="descricao">
+              <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ratione corporis temporibus enim dicta in nemo voluptatem mollitia repellendus laborum? Quam ut nulla excepturi voluptatum non provident odit libero. Maxime.
+              Officia in consectetur perferendis ipsum autem eos labore quos saepe dicta! Eligendi, eum cumque sequi aspernatur maxime repellat, saepe ratione veniam cupiditate eos sunt animi culpa vel quas. Ullam, eaque?
+              Animi quibusdam ipsa voluptatem eveniet exercitationem provident, voluptate earum debitis architecto esse cumque nemo iure voluptates est officiis deserunt rerum velit. Dolores voluptatem perferendis repellendus soluta sapiente recusandae natus libero.
+              Provident fuga officia aspernatur perspiciatis nesciunt illo, vitae, molestias soluta, maiores reprehenderit voluptas molestiae quod. Aspernatur quod eveniet illum aliquam voluptatem, blanditiis deleniti corrupti voluptatibus unde laudantium a dolorem odio.
+              Possimus non est minima dolorum eum facilis eligendi, dolor dolores! Dolorum reprehenderit quos eveniet obcaecati similique veniam molestiae placeat amet ullam, autem itaque ea asperiores magnam explicabo, distinctio eum deserunt.
+           0  </h6>
             </div>
-            <span id="divisoria"></span>
-            <div className="quadro col-lg-7" id="estagio">
-              <div className="d-flex align-items-center">
-                <img src={logoPresidencia} alt="Presidência da República"/>
-                <div className="d-flex flex-column">
-                  <p className="mb-0"><b>Estágio:</b></p>
-                  <p className="mb-0"><em>Diretoria de Acompanhamento da Execução Orçamentária (DEO)</em></p>
-                  <p className="mb-0"><em>Secretaria Especial de Acompanhamento Governamental (SEAG)</em></p>
-                  <p className="mb-0"><em>Secretaria de Relações Institucionais da Presidência da República (SRI – PR)</em></p>
-                </div>
-              </div>
-              <div className="mt-2">
-                <p className="mb-0"><b>Função:</b> Desenvolvedor Full Stack</p>
-                <p className="mb-0"><b>Atividade:</b> Auxiliar no desenvolvimento de soluções de TI, para automatizar processos
-                da secretaria de Relações Institucionais, da Presidência da República.</p>
-              </div>
-            </div>
-            <div id="descricao">
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam est perferendis quisquam voluptate suscipit nesciunt incidunt assumenda quod voluptatibus adipisci mollitia consequuntur quas, cumque quia, eos fuga ut eaque? Sint?</p>
+            <div className="col-lg-6" id="carreira">
+
             </div>
           </div>
         </div>
-        
-      </div>      
+      </div>
     );
   };
   
